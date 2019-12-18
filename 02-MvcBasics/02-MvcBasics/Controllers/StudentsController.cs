cd00ad5e5be50dd167e7611031a357d32c3278d3
Stann <stoychev.st2@gmail.com>
2019-12-18 08:37:24 +0200

    Added mvc demo proj

---
+++ 02-MvcBasics/02-MvcBasics/Controllers/StudentsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MvcBasics.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcBasics.Controllers
+{
+    public class StudentsController : Controller
+    {
+        Dictionary<int, Student> studentsDict;
+
+        public StudentsController()
+        {
+            this.studentsDict = new Dictionary<int, Student>();
+            studentsDict[1] = new Student() { FirstName = "Ivan", LastName = "Ivanov", Age = 23 };
+            studentsDict[2] = new Student() { FirstName = "Georgi", LastName = "Petrov", Age = 23 };
+        }
+
+        public IActionResult Index()
+        {
+            var students = new List<Student>();
+            foreach (var item in studentsDict)
+            {
+                students.Add(item.Value);
+            }
+            return View("IndexStudents", students);
+        }
+
+        public IActionResult ById(int id)
+        {
+            return View(model: studentsDict[id]);
+        }
+    }
+}
---
+++ 02-MvcBasics/02-MvcBasics/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcBasics.Models
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}

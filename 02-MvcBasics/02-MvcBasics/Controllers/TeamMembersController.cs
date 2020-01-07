6ef9b70de623e2641356a8616881897ea0ebfd9e
Stann <stoychev.st2@gmail.com>
2020-01-07 23:04:38 +0200

    Added working with data demo code

---
+++ 02-MvcBasics/02-MvcBasics/Controllers/TeamMembersController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MvcBasics.Models;
+
+namespace MvcBasics.Controllers
+{
+    public class TeamMembersController : Controller
+    {
+        public static List<TeamMember> team = new List<TeamMember>();
+
+        public IActionResult Index()
+        {
+            ViewData["title"] = "Team members list";
+            if(team.Count == 0)
+            {
+                team.Add(new TeamMember { Email = "ivan@abv.bg", FirstName = "Ivan", LastName = "Ivanov" });
+                team.Add(new TeamMember { Email = "gosho@abv.bg", FirstName = "Georgi", LastName = "Petrov" });
+            }
+            return View(team);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewData["Title"] = "Add team member";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(TeamMember created)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(created);
+            }
+
+            team.Add(created);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ 02-MvcBasics/02-MvcBasics/Models/TeamMember.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcBasics.Models
+{
+    public class TeamMember
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string LastName { get; set; }
+
+        [RegularExpression(@"\w+@\w+\.\w+", ErrorMessage = "Not a valid email")]
+        public string Email { get; set; }
+    }
+}
